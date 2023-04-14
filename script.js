d5a1b97f2967ab45af043880562ba8b2b1ac540a
nsamedaisy <nsamedaisy@gmail.com>
2023-04-14 04:19:56 +0100

    incomplete stopwatch

--- script.js
+++ script.js
@@ -1,37 +1,36 @@
 let [seconds, minutes, hours] = [0, 0, 0];
-let displayTime = document.getElementById("display-time");
-let timer = null;
+let displayTime = document.querySelector("display-time");
+let currentInterval = null;
 
-function stopWatch() {
-    seconds++;
-    if(seconds == 60) {
-        seconds = 0;
-        minutes++;
-        if(minutes == 60) {
-            minutes = 0;
-            hours++;
-        }
+document.getElementById('startWatch').addEventListener('click', () => {
+  if(currentInterval !== null) {
+    clearInterval(clearInterval);
     }
-    let h = hours < 10 ? "0" + hours: hours;
-    let m = minutes < 10 ? "0" + minutes: minutes;
-    let s = seconds < 10 ? "0" + seconds: seconds;
-}
-displayTime.innerHTML = h + ":" + m + ":" + s;
+    currentInterval = setInterval(displayTime, 10);
+});
 
-function startWatch() {
-    if (timer !== null) {
-        clearInterval(timer);
-    }
-    timer = setInterval (stopWatch, 1000);
-}
+document.getElementById('pauseWatch').addEventListener('click', () => {
+  clearInterval(clearInterval);
+});
 
-function stopWatch() {
-    clearInterval(timer);
-}
+document.getElementById('resetWatch').addEventListener('click', () => {
+  clearInterval(clearInterval);
+	[seconds, minutes, hours] = [0, 0, 0];
+	displayTime.innerHTML = '00 : 00 : 00';
+});
 
-function resetWatch() {
-    clearInterval(timer);
-
-    [seconds, minutes, hours] = [0,0,0];
-    displayTime.innerHTML = "00 : 00 : 00";
+function displayTime() {
+  seconds++;
+  if(seconds == 60) {
+    seconds = 0;
+    minutes++;
+    if(minutes == 60) {
+      minutes = 0;
+      hours++;
+   }
+  }
+  let h = hours < 10 ? "0" + hours: hours;
+  let m = minutes < 10 ? "0" + minutes: minutes;
+  let s = seconds < 10 ? "0" + seconds: seconds;
+  displayTime.innerHTML = h + ":" + m + ":" + s;
 }
