9c3a50e7f59ed7e9ab088028d232942c2d1187cf
nsamedaisy <nsamedaisy@gmail.com>
2023-04-19 10:18:02 +0100

    edits on code format script js

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
-let [seconds, minutes, hours] = [0, 0, 0];
-let displayTimer = document.getElementById("display-time");
-let currentInterval = null;
+let [seconds, minutes, hours] = [0, 0, 0]
+const displayTimer = document.getElementById('display-time')
+let currentInterval = null
 
 document.getElementById('pause-watch').addEventListener('click', () => {
-  clearInterval(currentInterval);
-});
+  clearInterval(currentInterval)
+})
 
 document.getElementById('reset-watch').addEventListener('click', () => {
   clearInterval(currentInterval);
-  [seconds, minutes, hours] = [0, 0, 0];
-  displayTimer.innerHTML = '00 : 00 : 00';
-});
+  [seconds, minutes, hours] = [0, 0, 0]
+  displayTimer.innerHTML = '00 : 00 : 00'
+})
 
-function displayTime() {
-  seconds++;
-  if (seconds == 60) {
-    seconds = 0;
-    minutes++;
-    if (minutes == 60) {
-      minutes = 0;
-      hours++;
+function displayTime () {
+  seconds++
+  if (seconds === 60) {
+    seconds = 0
+    minutes++
+    if (minutes === 60) {
+      minutes = 0
+      hours++
     }
   }
-  let h = hours < 10 ? "0" + hours : hours;
-  let m = minutes < 10 ? "0" + minutes : minutes;
-  let s = seconds < 10 ? "0" + seconds : seconds;
-  displayTimer.innerHTML = h + ":" + m + ":" + s;
+  const h = hours < 10 ? '0' + hours : hours
+  const m = minutes < 10 ? '0' + minutes : minutes
+  const s = seconds < 10 ? '0' + seconds : seconds
+  displayTimer.innerHTML = h + ':' + m + ':' + s
 }
 
 document.getElementById('start-watch').addEventListener('click', () => {
   if (currentInterval !== null) {
-    clearInterval(currentInterval);
+    clearInterval(currentInterval)
   }
-  currentInterval = setInterval(displayTime, 1000);
+  currentInterval = setInterval(displayTime, 1000)
   console.log(currentInterval)
-});
+})
